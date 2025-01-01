9c5d52ba71457e2f4e97a985e903fdfe5611be1a
Carlos Rodriguez <imindloop@gmail.com>
2024-12-31 20:45:06 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+#microsoft python real world applications
+
---
+++ datatypes-in-python.py
@@ -0,0 +1,25 @@
+# Python is a dynamic typed language, or in other words, the data type
+# accomodates to the data stored in variables
+
+# naming conventions
+    #Case sensitive
+    #can have any letter, number and underscore
+    #but can't start with a number
+
+# prints <class 'int'>
+x = 1
+print(type(x))
+
+# prints <class 'float'>
+y = 1.0
+print(type(y))
+
+# Boolean can hold True (1 in Int) or False (0 in Int)
+# prints <class 'bool'>
+t = True
+print(type(t))
+
+# string preferably are declared with single quotes, but double
+# can be used too
+name = 'Carlos Rodriguez'
+print(type(name))
